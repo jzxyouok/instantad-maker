2ab3cafdd577771f6473bc2b442b7735e9dd4fc4
jackyqi <jackyqi@tencent.com>
2016-12-05 20:45:46 +0800

    删除welcome，登录模块更新

--- src/module/login/login.js
+++ src/module/login/login.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './app'
 
 
+Vue.use(Vuex);
 
-
+import Lib from 'assets/Lib';
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
   components: { App }
-})
+  // created:function() {
+  //   Lib.$.ajax({
+  //     url:'http://x.addev.com/api/userinfo',
+  //     type:'get',
+  //     data:findGetParameter('ticket')?'ticket='+findGetParameter('ticket'):'',
+  //     crossdomain:true,
+  //     success:function(ret) {
+  //       if(ret.user) {
+  //         console.log(ret.user);
+  //       }
+  //     },failed:function() {
+  //       console.log('Get UserInfo Error');
+  //     }
+  //   });
+  //   console.log('created App');
+  // }
+});
